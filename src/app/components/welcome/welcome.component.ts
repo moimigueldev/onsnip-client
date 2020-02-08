d8ec435e00623ef4ae33cb9d9f8904be2e093381
moises <moimigueldev@gmail.com>
2020-02-08 12:31:20 -0600

    styled welcome page

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(2000, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private spotifyService: SpotifyService
+  ) { }
 
   ngOnInit() {
+    document.body.classList.add('welcome-bg-image');
+
+  }
+
+
+  onLogin(): void {
+    console.log('hello')
+    // this.userService.getUserData();
+  }
+
+  ngOnDestroy() {
+
+    //Removes BG image class
+    let classList = document.body.classList;
+    while (classList.length > 0) {
+      classList.remove(classList.item(0));
+    }
   }
 
 }
